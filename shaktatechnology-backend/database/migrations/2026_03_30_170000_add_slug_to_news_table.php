132abb0eace300bd94dac7ce700e2ee3e10343d4
Damodar Khanal <deepakkhanal931@gmail.com>
2026-05-20 06:05:22 +0545

    changes are made

--- shaktatechnology-backend/database/migrations/2026_03_30_170000_add_slug_to_news_table.php
+++ shaktatechnology-backend/database/migrations/2026_03_30_170000_add_slug_to_news_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('news', function (Blueprint $table) {
-            $table->string('slug')->unique()->after('title')->nullable();
+            $table->string('    ')->unique()->after('title')->nullable();
         });
     }
 
